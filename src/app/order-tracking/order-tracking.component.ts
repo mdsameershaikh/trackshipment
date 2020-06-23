add379577f9b786739fb53ac8a5bc9e0908b4d5a
sameer shaikh <sameershaikh@gofynd.com>
2020-06-23 14:18:05 +0530

    basicauth change

--- src/app/order-tracking/order-tracking.component.ts
+++ src/app/order-tracking/order-tracking.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {OrderdataService } from '../orderdata.service'
 import { ActivatedRoute } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -19,19 +20,20 @@ export class OrderTrackingComponent implements OnInit  {
   constructor(private service: OrderdataService,
     private route: ActivatedRoute
     ) { }
-
+   
 
   ngOnInit(): void {
+    let authorizationData = 'Basic ' + btoa('fynd' + ':' + "Fynd123");
         let t = this.route.snapshot.paramMap.get('order_id');
+        
+     
     // alert(t);
     this.orderid = t;
     this.showShipments(this.orderid);
   }
 
 
-
   
-
     showShipments(orderid) {
       this.service.getShipment(orderid).subscribe(data => { 
         this.responseData = data.results})}
--- src/app/orderdata.service.ts
+++ src/app/orderdata.service.ts
@@ -1,28 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 
 @Injectable()
 export class OrderdataService {
-
   constructor(private httpClient:HttpClient) { }
 
-  
-  createAuthorizationHeader(headers: Headers)  {
-    headers.append('Authorization', 'Basic ' +
-      btoa('fynd:Fynd123')); 
-      // check if space is needed 
-  }
-  
-  getShipment(orderid):Observable<any>{
-    let headers = new Headers()
-    this.createAuthorizationHeader(headers);
-    let url =  "https://workbench.fynd.com/workbench-service/api/v1/hydra/groups/open/apis/shipment_track_ss/execute?order_id="+ orderid;
-    return this.httpClient.get(url, {
-      headers: headers
-    });  }
+ httpOptions = {
+   headers: new HttpHeaders({
+     'content-Type': 'application/json',
+     'Authorization': 'Basic ' + btoa('fynd:Fynd123')
+   })
+ }
 
+  getShipment(orderid):Observable<any>{
+    let url =  "https://workbench.fynd.com/workbench-service/api/v1/hydra/groups/open/apis/shipment_track_ss/execute?order_id="+ orderid;
+    return this.httpClient.get(url, this.httpOptions);  }
   getProgress(clickedShipment):Observable<any>{
     let url = "https://api.fynd.com/computron/api/v1/store/shipment-bags/?shipment_id="+clickedShipment;
     return this.httpClient.get(url);
